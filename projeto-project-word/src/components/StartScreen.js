4418bff05fafc3d8e842cd119a84fdf5c0509a66
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2022-11-16 09:33:02 -0300

    .
    .

--- projeto-project-word/src/components/StartScreen.js
+++ projeto-project-word/src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = ({StartGame}) => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
         <h1>Secret Word</h1>
         <p>Clique no botão para iniciar o game!</p>
-        <button onClick={StartGame}>Começar</button>
+        <button onClick={startGame}>Começar</button>
     </div>
   )
 }
